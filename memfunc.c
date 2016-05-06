727c83a5e96f58fbf6bc0c8726db6dc91f586abe
Miguel Herrnsdorf <herrdragon@users.noreply.github.com>
2016-05-06 08:44:32 -0400

    Update memfunc.c

--- memfunc.c
+++ memfunc.c
@@ -1,11 +1,6 @@
 // memfunc.c
 /******************************************************************************/
-/* PROGRAMMER:  Luis M. Herrnsdorf  
-   ID: 5481644
-   CLASS:       COP4338 U03   TT 7:50
-   INSTRUCTOR:  William Feild
-   ASSIGNMENT:  #6.   DUE Thursday 4/28
-*/
+/* PROGRAMMER:  Miguel Herrnsdorf */
 /*DESCRIPTION: functions used by process.c
 */
 /* 
